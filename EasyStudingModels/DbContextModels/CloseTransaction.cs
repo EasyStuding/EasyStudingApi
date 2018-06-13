baa319fe99cbd21cdf2d35cafea9e8570fac8c86
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-13 16:26:57 +0300

    Added close transaction.

---
+++ EasyStudingModels/DbContextModels/CloseTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyStudingModels.DbContextModels
+{
+    public partial class CloseTransaction
+    {
+        public long Id { get; set; }
+        public long CustomerId { get; set; }
+        public long ExecutorId { get; set; }
+        public bool? IsClosedByCustomer { get; set; }
+        public bool? IsClosedByExecutor { get; set; }
+    }
+}
