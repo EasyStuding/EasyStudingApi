8c52c885a8665462f299f80f89538312f6481a0e
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-13 23:23:53 +0300

    fix name of dbset.

--- EasyStudingRepositories/DbContext/EasyStudingContext.cs
+++ EasyStudingRepositories/DbContext/EasyStudingContext.cs
@@ -23,7 +23,7 @@ namespace EasyStudingRepositories.DbContext
         public virtual DbSet<Country> Countries { get; set; }
         public virtual DbSet<Education> Educations { get; set; }
         public virtual DbSet<EducationType> EducationTypes { get; set; }
-        public virtual DbSet<EducationUserDecription> EducationUserDecriptions { get; set; }
+        public virtual DbSet<EducationUserDescription> EducationUserDecriptions { get; set; }
         public virtual DbSet<EmailDescription> EmailDescriptions { get; set; }
         public virtual DbSet<ExecutorSkill> ExecutorSkills { get; set; }
         public virtual DbSet<OpenSource> OpenSources { get; set; }
@@ -162,7 +162,7 @@ namespace EasyStudingRepositories.DbContext
                     .HasColumnName("typename");
             });
 
-            modelBuilder.Entity<EducationUserDecription>(entity =>
+            modelBuilder.Entity<EducationUserDescription>(entity =>
             {
                 entity.ToTable("educationuserdecriptions");
 
