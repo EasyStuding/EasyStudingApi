471a17f47c8f551b290c7d3c5874ce1cb6061af5
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-13 20:42:20 +0300

    Added interface to repository.

--- EasyStudingInterfaces/Repositories/IRepository.cs
+++ EasyStudingInterfaces/Repositories/IRepository.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EasyStudingInterfaces.Repositories
 {
-    public interface IRepository
+    public interface IRepository<T>
     {
+        IQueryable<T> GetAll();
 
+        T Get(long id);
+
+        T Add(T param);
+
+        T Edit(T param);
+
+        T Remove(T param);
     }
 }
