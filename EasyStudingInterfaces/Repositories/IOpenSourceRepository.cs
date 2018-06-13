b4e3659449bcb06177116e2ba92448ab6e003a35
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-13 16:28:14 +0300

    initial interfaces of repositories controllers.

---
+++ EasyStudingInterfaces/Repositories/IOpenSourceRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyStudingInterfaces.Repositories
+{
+    public interface IOpenSourceRepository
+    {
+    }
+}
