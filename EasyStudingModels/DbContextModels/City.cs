35f4e07058256be53bdeb548a74d08a6007a8d0a
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-15 20:36:13 +0300

    Edited city model(add region).

--- EasyStudingModels/DbContextModels/City.cs
+++ EasyStudingModels/DbContextModels/City.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public long CountryId { get; set; }
+        public string Region { get; set; }
         public string Name { get; set; }
     }
 }
