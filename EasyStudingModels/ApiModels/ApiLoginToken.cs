8a86c6833c8c1fb3670573d6e9a59ee988876edc
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-15 20:23:22 +0300

    Added api models to registration and login.

---
+++ EasyStudingModels/ApiModels/ApiLoginToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyStudingModels.ApiModels
+{
+    public class ApiLoginToken
+    {
+    }
+}
---
+++ EasyStudingModels/ApiModels/ApiRegisrtationLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyStudingModels.ApiModels
+{
+    public class ApiRegisrtationLoginModel
+    {
+        public long UserRegistrationId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
