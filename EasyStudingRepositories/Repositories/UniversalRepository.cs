ef66c5ebe7e76bd5957a67f863ac81b95cf6e4dd
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-21 16:21:29 +0300

    Change universal repository and tests for this repository.

---
+++ EasyStudingRepositories/Repositories/UniversalRepository.cs
@@ -0,0 +1,83 @@
+﻿using EasyStudingInterfaces.Repositories;
+using EasyStudingInterfaces.Services;
+using EasyStudingModels;
+using EasyStudingRepositories.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyStudingRepositories.Repositories
+{
+    public class UniversalRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IEntity<TEntity>
+    {
+        private DbSet<TEntity> _dbSet;
+        private EasyStudingContext _context;
+
+        public UniversalRepository(EasyStudingContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public async Task<TEntity> GetAsync(long id)
+        {
+            var entity = await _dbSet.FirstOrDefaultAsync(e => e.Id == id);
+
+
+            if(entity == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return entity;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            await _dbSet.AddAsync(param);
+
+            await _context.SaveChangesAsync();
+
+            return _dbSet.LastOrDefault();
+        }
+
+        public async Task<TEntity> EditAsync(TEntity param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var entity = await GetAsync(param.Id);
+
+            entity.Edit(param);
+
+            await _context.SaveChangesAsync();
+
+            return _dbSet.FirstOrDefault(e => e.Id == param.Id);
+        }
+
+        public async Task<TEntity> RemoveAsync(long id)
+        {
+            var entity = await GetAsync(id);
+
+            _dbSet.Remove(entity);
+
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
---
+++ EasyStudingUnitTests/RepositoryTests/UniversalRepositoryTest.cs
@@ -0,0 +1,126 @@
+﻿using EasyStudingRepositories.DbContext;
+using EasyStudingRepositories.Repositories;
+using EasyStudingModels.DbContextModels;
+using EasyStudingUnitTests.TestData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+
+namespace EasyStudingUnitTests.RepositoryTests
+{
+    public class UniversalRepositoryTest
+    {
+        private EasyStudingContext Context;
+
+        [Fact(DisplayName = "UniversalRepository.GetAll() should return 5 objects.")]
+        public void UniversalRepository_GetAll_should_return_5_objects()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var result = rep.GetAll();
+
+                Assert.Equal(5, result.Count());
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Get(1) should return first object.")]
+        public async void UniversalRepository_Get_1_should_return_first_object()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var result = await rep.GetAsync(1);
+
+                Assert.Equal(1, result.Id);
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Add(model) should return valid model.")]
+        public async void UniversalRepository_Add_model_should_return_valid_model()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var model = await rep.AddAsync(new City() { Id = 6 });
+
+                Assert.Equal(6, model.Id);
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Add(null) should return argument null exception.")]
+        public async void UniversalRepository_Add_null_should_return_argument_null_exception()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var ex = await Assert.ThrowsAsync<ArgumentNullException>(async () => await rep.AddAsync(null));
+
+                Assert.Equal(typeof(ArgumentNullException), ex.GetType());
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Edit(model) should return valid model.")]
+        public async void UniversalRepository_Edit_model_should_return_valid_model()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var model = await rep.EditAsync(new City() { Id = 5 });
+
+                Assert.Equal(5, model.Id);
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Edit(null) should return argument null exception.")]
+        public async void UniversalRepository_Edit_null_should_return_argument_null_exception()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var ex = await Assert.ThrowsAsync<ArgumentNullException>(async () => await rep.EditAsync(null));
+
+                Assert.Equal(typeof(ArgumentNullException), ex.GetType());
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Edit(7) should return index out of range exception.")]
+        public async void UniversalRepository_Edit_7_should_return_index_out_of_range_exception()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var ex = await Assert.ThrowsAsync<IndexOutOfRangeException>(async () => await rep.EditAsync(new City() { Id = 7 }));
+
+                Assert.Equal(typeof(IndexOutOfRangeException), ex.GetType());
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Remove(model) should return valid model.")]
+        public async void UniversalRepository_Remove_model_should_return_valid_model()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var model = await rep.RemoveAsync(5);
+
+                Assert.Equal(5, model.Id);
+            }
+        }
+
+        [Fact(DisplayName = "UniversalRepository.Remove(7) should return index out of range exception.")]
+        public async void UniversalRepository_Remove_7_should_return_index_out_of_range_exception()
+        {
+            using (Context = new TestDbContext().Context)
+            {
+                var rep = new UniversalRepository<City>(Context);
+                var ex = await Assert.ThrowsAsync<IndexOutOfRangeException>(async () => await rep.RemoveAsync(7));
+
+                Assert.Equal(typeof(IndexOutOfRangeException), ex.GetType());
+            }
+        }
+    }
+}
