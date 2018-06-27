19710dcb81b2fc124f1b9de24719f9199b2f883f
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-27 19:12:49 +0300

    Added extension what checked model for exceptions.

---
+++ EasyStudingModels/Extensions/ValidateModelsExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyStudingModels.Extensions
+{
+    public static class ValidateModelsExtension
+    {
+        public static void CheckArgumentException(this IValidatedEntity entity)
+        {
+            if (entity == null
+                || !entity?.Validate() == true)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
