c2fd2c674883ed24eb5a6ba22920643dad60d505
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-28 21:27:34 +0300

    Added log service to writes logs.

---
+++ EasyStudingServices/LogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyStudingServices
+{
+    public static class LogService
+    {
+        public static void UpdateLogFile(Exception ex)
+        {
+            var date = DateTime.Now.ToString("dd/MM/yy HH:mm:ss");
+            var res = date + " --- " + ex.Message;
+
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+                res += ex.Message;
+            }
+
+            File.AppendAllText("log.txt", res);
+        }
+    }
+}
