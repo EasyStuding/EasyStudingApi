13dc267516546d3e19fe5bbf66dbac698e2ab3d8
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-06-28 21:26:23 +0300

    added try/catch block(need for tests).

--- EasyStudingServices/SmsService.cs
+++ EasyStudingServices/SmsService.cs
@@ -13,17 +13,22 @@ namespace EasyStudingServices
         private const string ACCOUNT_SID = "AC7155309318a997f1487d24b5a5a422a7";
         private const string AUTH_TOKEN = "14fea565bf80af8a8385292b0130e357";
 
-        public static string Send(string telephoneNumber, string code)
+        public static void Send(string telephoneNumber, string code)
         {
-            TwilioClient.Init(ACCOUNT_SID, AUTH_TOKEN);
+            try
+            {
+                TwilioClient.Init(ACCOUNT_SID, AUTH_TOKEN);
 
-            var to = new PhoneNumber(telephoneNumber);
-            var message = MessageResource.Create(
-                to,
-                from: new PhoneNumber(FROM_NUMBER),
-                body: $"EasyStuding code: {code}. Valid for 3 minutes.");
-
-            return message.Sid;
+                var to = new PhoneNumber(telephoneNumber);
+                var message = MessageResource.Create(
+                    to,
+                    from: new PhoneNumber(FROM_NUMBER),
+                    body: $"EasyStuding code: {code}. Valid for 3 minutes.");
+            }
+            catch(Exception ex)
+            {
+                LogService.UpdateLogFile(ex);
+            }
         }
     }
 }
