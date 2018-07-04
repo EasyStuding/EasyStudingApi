494f33eb89cca08ca9eefe0cb7837853625cb0e6
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-04 13:36:06 +0300

    Edit log service.

--- EasyStudingServices/LogService.cs
+++ EasyStudingServices/LogService.cs
@@ -2,11 +2,15 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace EasyStudingServices
 {
     public static class LogService
     {
+        private static readonly List<string> _notWrittenExceptions
+            = new List<string>();
+
         public static void UpdateLogFile(Exception ex)
         {
             var date = DateTime.Now.ToString("dd/MM/yy HH:mm:ss");
@@ -21,8 +25,23 @@ namespace EasyStudingServices
             var path = Path.Combine(
                            Directory.GetCurrentDirectory(),
                            "wwwroot", DateTime.Now.ToString("dd_MM_yy") + "_log.txt");
+            try
+            {
+                if (_notWrittenExceptions.Count > 0)
+                {
+                    while (_notWrittenExceptions.Count > 0)
+                    {
+                        File.AppendAllText(path, _notWrittenExceptions.Last() + Environment.NewLine);
+                        _notWrittenExceptions.Remove(_notWrittenExceptions.Last());
+                    }
+                }
 
-            File.AppendAllText(path, res + Environment.NewLine);
+                File.AppendAllText(path, res + Environment.NewLine);
+            }
+            catch
+            {
+                _notWrittenExceptions.Add(res + Environment.NewLine);
+            }
         }
     }
 }
