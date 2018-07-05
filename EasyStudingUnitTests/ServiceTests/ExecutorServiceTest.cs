9812b59afe657bf04163f8a97dc047d590b4d016
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-05 17:49:30 +0300

    Added executor service tests.

---
+++ EasyStudingUnitTests/ServiceTests/ExecutorServiceTest.cs
@@ -0,0 +1,70 @@
+﻿using EasyStudingServices.Services;
+using EasyStudingUnitTests.TestData;
+using System.Linq;
+using Xunit;
+
+namespace EasyStudingUnitTests.ServiceTests
+{
+    public class ExecutorServiceTest
+    {
+        [Fact(DisplayName = "ExecutorService.GetOrders(valid) should return 5 objects.")]
+        public async void ExecutorService_GetOrders_valid_should_return_5_objects()
+        {
+            var service = new ExecutorService(new TestExecutorRepository());
+
+            var result = await service.GetOrders(null, null, null, null, 2);
+
+            Assert.Equal(5, result.Count());
+        }
+
+        [Fact(DisplayName = "ExecutorService.GetOrder(1, 2) should return valid object.")]
+        public async void ExecutorService_GetOrder_1_1_should_return_valid_object()
+        {
+            var service = new ExecutorService(new TestExecutorRepository());
+
+            var result = await service.GetOrder(1, 2);
+
+            Assert.Equal(1, result.Id);
+        }
+                
+        [Fact(DisplayName = "ExecutorService.GetTheRightsToPerformOrder(1, 2) should return valid object.")]
+        public async void ExecutorService_GetTheRightsToPerformOrder_1_2_should_return_valid_object()
+        {
+            var service = new ExecutorService(new TestExecutorRepository());
+
+            var result = await service.GetTheRightsToPerformOrder(1, 2);
+
+            Assert.Equal(2, result.ExecutorId);
+        }
+
+        [Fact(DisplayName = "ExecutorService.CloseOrder(1, 2) should return valid object.")]
+        public async void ExecutorService_CloseOrder_1_2_should_return_valid_object()
+        {
+            var service = new ExecutorService(new TestExecutorRepository());
+
+            var result = await service.CloseOrder(1, 2);
+
+            Assert.True(result.IsClosedByExecutor);
+        }
+
+        [Fact(DisplayName = "ExecutorService.AddSkill(1, 2) should return valid object.")]
+        public async void ExecutorService_AddSkill_1_2_should_return_valid_object()
+        {
+            var service = new ExecutorService(new TestExecutorRepository());
+
+            var result = await service.AddSkill(1, 2);
+
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact(DisplayName = "ExecutorService.RemoveSkill(1, 2) should return valid object.")]
+        public async void ExecutorService_RemoveSkill_1_2_should_return_valid_object()
+        {
+            var service = new ExecutorService(new TestExecutorRepository());
+
+            var result = await service.RemoveSkill(1, 2);
+
+            Assert.Equal(1, result.Id);
+        }
+    }
+}
---
+++ EasyStudingUnitTests/TestData/TestExecutorRepository.cs
@@ -0,0 +1,91 @@
+﻿using EasyStudingInterfaces.Repositories;
+using EasyStudingModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyStudingUnitTests.TestData
+{
+    public class TestExecutorRepository : IExecutorRepository
+    {
+        public async Task<IQueryable<Order>> GetOrders(string education, string country, string region, string city, long currentUserId)
+        {
+            return new[]
+            {
+                new Order(),
+                new Order(),
+                new Order(),
+                new Order(),
+                new Order()
+            }.AsQueryable();
+        }
+
+        public async Task<Order> GetOrder(long id, long currentUserId)
+        {
+            return new Order()
+            {
+                Id = 1,
+                CustomerId = 1,
+                ExecutorId = 2,
+                Description = "desc",
+                InProgress = true,
+                IsClosedByCustomer = false,
+                IsClosedByExecutor = false,
+                IsCompleted = false,
+                Title = "title"
+            };
+        }
+
+        public async Task<Order> GetTheRightsToPerformOrder(long id, long currentUserId)
+        {
+            return new Order()
+            {
+                Id = 1,
+                CustomerId = 1,
+                ExecutorId = 2,
+                Description = "desc",
+                InProgress = false,
+                IsClosedByCustomer = false,
+                IsClosedByExecutor = false,
+                IsCompleted = false,
+                Title = "title"
+            };
+        }
+
+        public async Task<Order> CloseOrder(long id, long currentUserId)
+        {
+            return new Order()
+            {
+                Id = 1,
+                CustomerId = 1,
+                ExecutorId = 2,
+                Description = "desc",
+                InProgress = true,
+                IsClosedByCustomer = false,
+                IsClosedByExecutor = true,
+                IsCompleted = false,
+                Title = "title"
+            };
+        }
+
+        public async Task<Skill> AddSkill(long id, long currentUserId)
+        {
+            return new Skill
+            {
+                Id = 1,
+                Name = "SQL"
+            };
+        }
+
+        public async Task<Skill> RemoveSkill(long id, long currentUserId)
+        {
+            return new Skill
+            {
+                Id = 1,
+                Name = "SQL"
+            };
+        }
+    }
+}
