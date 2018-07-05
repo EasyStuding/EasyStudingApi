199356b688b3013896dc123355776fdbcf3ef1c3
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-05 14:51:32 +0300

    Realize interface of IExecutorRepository.

--- EasyStudingInterfaces/Repositories/IExecutorRepository.cs
+++ EasyStudingInterfaces/Repositories/IExecutorRepository.cs
@@ -1,12 +1,21 @@
 ﻿using EasyStudingModels.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EasyStudingInterfaces.Repositories
 {
     public interface IExecutorRepository
     {
+        Task<IQueryable<Order>> GetOrders(string education, string country, string region, string city, long currentUserId);
+
+        Task<Order> GetOrder(long id, long currentUserId);
+
+        Task<Order> GetTheRightsToPerformOrder(long id, long currentUserId);
+
+        Task<Order> CloseOrder(long id, long currentUserId);
+
+        Task<Skill> AddSkill(long id, long currentUserId);
+
+        Task<Skill> RemoveSkill(long id, long currentUserId);
     }
 }
