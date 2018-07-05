78710f45169c50748625f9d83514ec722d0c78fb
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-05 17:15:45 +0300

    Realize executor repository.

--- EasyStudingRepositories/Repositories/ExecutorRepository.cs
+++ EasyStudingRepositories/Repositories/ExecutorRepository.cs
@@ -1,6 +1,7 @@
 ﻿using EasyStudingInterfaces.Repositories;
 using EasyStudingModels.Models;
 using EasyStudingRepositories.DbContext;
+using EasyStudingRepositories.Extensions;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace EasyStudingRepositories.Repositories
         private readonly IRepository<User> _userRepository;
         private readonly IRepository<Order> _orderRepository;
         private readonly IRepository<Skill> _skillRepository;
+        private readonly IRepository<UserSkill> _userSkillRepository;
 
         #endregion
 
@@ -25,6 +27,7 @@ namespace EasyStudingRepositories.Repositories
             _userRepository = new UniversalRepository<User>(_context);
             _orderRepository = new UniversalRepository<Order>(_context);
             _skillRepository = new UniversalRepository<Skill>(_context);
+            _userSkillRepository = new UniversalRepository<UserSkill>(_context);
         }
 
         /// <summary>
@@ -39,10 +42,24 @@ namespace EasyStudingRepositories.Repositories
         ///    Orders sorted by city and education.
         /// </returns>
         /// <exception cref="System.UnauthorizedAccessException">Current user not executor.</exception>
+        /// <exception cref="System.ArgumentNullException">When result not found.</exception>
 
         public async Task<IQueryable<Order>> GetOrders(string education, string country, string region, string city, long currentUserId)
         {
-            throw new Exception();
+            (await _userRepository.GetAsync(currentUserId)).CheckExecutorSubscription();
+
+            var users = _userRepository.GetAll().Where(u =>
+                u.Education.Contains(education)
+                && u.Country.Contains(country)
+                && u.Region.Contains(region)
+                && u.City.Contains(city))
+                ?? throw new ArgumentNullException();
+
+            return _orderRepository.GetAll().Where(o =>
+                o.ExecutorId == null
+                && users.Any(u =>
+                    u.Id == o.CustomerId))
+                ?? throw new ArgumentNullException();
         }
 
         /// <summary>
@@ -53,12 +70,13 @@ namespace EasyStudingRepositories.Repositories
         /// <returns>
         ///    Requsted order.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">When result null.</exception>
-        /// <exception cref="System.UnauthorizedAccessException">Id of executor != currentUserId or current user not executor.</exception>
+        /// <exception cref="System.UnauthorizedAccessException">Current user not executor.</exception>
 
         public async Task<Order> GetOrder(long id, long currentUserId)
         {
-            throw new Exception();
+            (await _userRepository.GetAsync(currentUserId)).CheckExecutorSubscription();
+
+            return await _orderRepository.GetAsync(id);
         }
 
         /// <summary>
@@ -70,11 +88,21 @@ namespace EasyStudingRepositories.Repositories
         ///    Requsted order.
         /// </returns>
         /// <exception cref="System.InvalidOperationException">When order have executor.</exception>
-        /// <exception cref="System.UnauthorizedAccessException">Current user not executor.</exception>
+        /// <exception cref="System.UnauthorizedAccessException">Id of executor != currentUserId 
+        /// or current user not executor.</exception>
 
         public async Task<Order> GetTheRightsToPerformOrder(long id, long currentUserId)
         {
-            throw new Exception();
+            (await _userRepository.GetAsync(currentUserId)).CheckExecutorSubscription();
+
+            var order = await _orderRepository.GetAsync(id);
+
+            order.ExecutorId = (order.ExecutorId == null
+                && order.InProgress == false)
+                ? currentUserId
+                : throw new InvalidOperationException();
+
+            return await _orderRepository.EditAsync(order);
         }
 
         /// <summary>
@@ -85,11 +113,24 @@ namespace EasyStudingRepositories.Repositories
         /// <returns>
         ///   Requested order.
         /// </returns>
-        /// <exception cref="System.UnauthorizedAccessException">Current user not executor.</exception>
+        /// <exception cref="System.UnauthorizedAccessException">Current user not executor 
+        /// or executor of order not current user.</exception>
 
         public async Task<Order> CloseOrder(long id, long currentUserId)
         {
-            throw new Exception();
+            (await _userRepository.GetAsync(currentUserId)).CheckExecutorSubscription();
+
+            var order = await _orderRepository.GetAsync(id);
+
+            order.IsClosedByExecutor = order.ExecutorId == currentUserId
+                ? true
+                : throw new UnauthorizedAccessException();
+
+            order.IsCompleted = order.IsClosedByCustomer == true
+                ? true
+                : false;
+
+            return await _orderRepository.EditAsync(order);
         }
 
         /// <summary>
@@ -104,7 +145,17 @@ namespace EasyStudingRepositories.Repositories
 
         public async Task<Skill> AddSkill(long id, long currentUserId)
         {
-            throw new Exception();
+            (await _userRepository.GetAsync(currentUserId)).CheckExecutorSubscription();
+
+            var skill = await _skillRepository.GetAsync(id);
+
+            await _userSkillRepository.AddAsync(new UserSkill()
+            {
+                SkillId = skill.Id,
+                UserId = currentUserId
+            });
+
+            return skill;
         }
 
         /// <summary>
@@ -116,10 +167,23 @@ namespace EasyStudingRepositories.Repositories
         ///    Removed skill.
         /// </returns>
         /// <exception cref="System.UnauthorizedAccessException">Current user not executor.</exception>
+        /// <exception cref="System.ArgumentNullException">Result not found.</exception>
 
         public async Task<Skill> RemoveSkill(long id, long currentUserId)
         {
-            throw new Exception();
+            (await _userRepository.GetAsync(currentUserId)).CheckExecutorSubscription();
+
+            var skill = await _skillRepository.GetAsync(id);
+
+            var userSkill = _userSkillRepository
+                .GetAll()
+                .FirstOrDefault(us => us.SkillId == skill.Id
+                    && us.UserId == currentUserId)
+                ?? throw new ArgumentNullException();
+
+            await _userSkillRepository.RemoveAsync(userSkill.Id);
+
+            return skill;
         }
     }
 }
