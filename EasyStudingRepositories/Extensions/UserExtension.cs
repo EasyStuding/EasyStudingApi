15f084c211578f534a1790a06e74374bbf2ba742
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-05 17:15:28 +0300

    Added extension to check executor subscription of user.

---
+++ EasyStudingRepositories/Extensions/UserExtension.cs
@@ -0,0 +1,17 @@
+﻿using EasyStudingModels.Models;
+using System;
+
+namespace EasyStudingRepositories.Extensions
+{
+    public static class UserExtension
+    {
+        public static void CheckExecutorSubscription(this User user)
+        {
+            if (user.SubscriptionExecutorExpiresDate == null
+                || user.SubscriptionExecutorExpiresDate <= DateTime.Now)
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
