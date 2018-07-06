c77f8330f765c9b570a2497aa71dfc442c995c6a
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-06 11:57:12 +0300

    Added hash passwords.

--- EasyStudingRepositories/Repositories/SessionRepository.cs
+++ EasyStudingRepositories/Repositories/SessionRepository.cs
@@ -102,8 +102,8 @@ namespace EasyStudingRepositories.Repositories
             userPassword = await _userPasswordRepository.AddAsync(new UserPassword()
             {
                 UserId = user.Id,
-                Password = loginModel.Password
-            }) ?? throw new InvalidOperationException();
+                Password = HashPassword(loginModel.Password)
+            });
 
             return user;
         }
@@ -134,7 +134,7 @@ namespace EasyStudingRepositories.Repositories
                 .FirstOrDefault(u => u.TelephoneNumber.Equals(loginModel.TelephoneNumber))
                 ?? throw new ArgumentNullException();
 
-            if (!user.Password.Equals(loginModel.Password))
+            if (!VerifyHashedPassword(user.Password, loginModel.Password))
             {
                 throw new InvalidOperationException();
             }
@@ -207,7 +207,7 @@ namespace EasyStudingRepositories.Repositories
             var password = GetUserPasswordByUserId(user.Id)
                 ?? throw new ArgumentNullException();
 
-            password.Password = restorePasswordModel.Password;
+            password.Password = HashPassword(restorePasswordModel.Password);
 
             var editedPassword = await _userPasswordRepository.EditAsync(password);
 
@@ -264,6 +264,54 @@ namespace EasyStudingRepositories.Repositories
                 .GetAll()
                 .FirstOrDefault(up =>
                     up.UserId == id);
+        }
+
+        //Create hash of password.
+        private static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+
+            byte[] dst = new byte[0x31];
+
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+
+            return Convert.ToBase64String(dst);
+        }
+
+        //Verify password.
+        private static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            
+            byte[] src = Convert.FromBase64String(hashedPassword);
+
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+
+            byte[] dst = new byte[0x10];
+
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+
+            byte[] buffer3 = new byte[0x20];
+
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+
+            return buffer3.SequenceEqual(buffer4);
         }
     }
 }
