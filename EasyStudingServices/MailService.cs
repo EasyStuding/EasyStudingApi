a6c1d758ed51fbca9b2630ad413ee1e1688ef3f1
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-10 12:38:25 +0300

    Update mail service.

--- EasyStudingServices/MailService.cs
+++ EasyStudingServices/MailService.cs
@@ -53,6 +53,46 @@ namespace EasyStudingServices
             }
         }
 
+        public static void Send(string email, string subject, string body)
+        {
+            try
+            {
+                using (var mail = new MailMessage())
+                {
+                    using (var SmtpServer = new SmtpClient())
+                    {
+                        mail.From = new MailAddress(CREDS.Item1);
+
+                        mail.To.Add(email);
+
+                        mail.Subject = subject;
+
+                        mail.Body = body;
+
+                        SmtpServer.Host = "smtp.gmail.com";
+
+                        SmtpServer.Port = 587;
+
+                        SmtpServer.EnableSsl = true;
+
+                        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                        SmtpServer.UseDefaultCredentials = false;
+
+                        SmtpServer.Credentials =
+                            new NetworkCredential(CREDS.Item1, CREDS.Item2);
+
+                        SmtpServer
+                            .Send(mail);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogService.UpdateLogFile(ex);
+            }
+        }
+
         private static string GetStringFromBase64(string str)
         {
             return Encoding
