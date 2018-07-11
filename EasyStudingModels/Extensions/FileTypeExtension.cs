0ec10c9c6b3780521932a748321a2905093be3dc
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-11 13:51:34 +0300

    closes #16

--- EasyStudingModels/Extensions/FileTypeExtension.cs
+++ EasyStudingModels/Extensions/FileTypeExtension.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using Microsoft.AspNetCore.StaticFiles;
 
 namespace EasyStudingModels.Extensions
 {
@@ -7,28 +6,10 @@ namespace EasyStudingModels.Extensions
     {
         public static string GetContentType(this string path)
         {
-            var types = GetMimeTypes();
-            var ext = Path.GetExtension(path).ToLowerInvariant();
-            return types[ext];
-        }
+            new FileExtensionContentTypeProvider()
+                .TryGetContentType(path, out string contentType);
 
-        private static Dictionary<string, string> GetMimeTypes()
-        {
-            return new Dictionary<string, string>
-            {
-                {".txt", "text/plain"},
-                {".pdf", "application/pdf"},
-                {".doc", "application/vnd.ms-word"},
-                {".docx", "application/vnd.ms-word"},
-                {".xls", "application/vnd.ms-excel"},
-                {".xlsx", @"application/vnd.openxmlformats
-                           officedocument.spreadsheetml.sheet"},
-                {".png", "image/png"},
-                {".jpg", "image/jpeg"},
-                {".jpeg", "image/jpeg"},
-                {".gif", "image/gif"},
-                {".csv", "text/csv"}
-            };
+            return contentType;
         }
     }
 }
